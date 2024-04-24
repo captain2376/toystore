5e6a6d07e18792718dddacd28daa501a0de7932d
sindhuja.d <sindhuja.d@cgg.gov.in>
2024-04-24 12:18:37 +0530

    24_4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
-import ListGroup from "./components/ListGroup";
 import Home from "./components/Pages/Home";
 import Footer from "./components/Footer";
-import SignUp from "./components/Pages/SignUp";
-
+ 
  function App() {
   return <div> 
-     <SignUp />
+     <Header/>
+     <Home />
+     <Footer />
    </div> 
  }
 
